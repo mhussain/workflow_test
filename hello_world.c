b3d9d96f625ea6bd638f28613de8da726309c338
Mujtaba Hussain <mujtaba.hussain@rea-group.com>
2014-05-19 22:56:23 +1000

    Saying hello in farsi

--- hello_world.c
+++ hello_world.c
@@ -6,5 +6,6 @@ int main(int argc, char **argv) {
   fprintf(stdout, "Kya sa\n");
   fprintf(stdout, "!Hola!\n");
   fprintf(stdout, "!GutenTag!\n");
+  fprintf(stdout, "Ahlan wa Sahlan\n");
   return 1;
 }
